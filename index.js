db5dc96a44c24afa25d612a4a4673b844fd4a030
savaliya krupal <krupalsavaliya0@gmail.com>
2024-08-16 10:48:38 +0530

    Your commit message for submodule

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/send-mail", async (req, res) => {
+    const {email, subject,name ,message}=req.body;
+    console.log(email)   
+  console.log("Server listening");
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", // Correct SMTP host for Gmail
+    port: 465, // Port for secure connections
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'krupalsavaliya0pvt@gmail.com', // Your Gmail address
+      pass: 'akqeyjfmcrdtvdre', // Your Gmail App Password
+    },
+  });
+
+  try {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: 'krupalsavaliya0pvt@gmail.com', // sender address
+      to: `  krupalsavaliya0@gmail.com , ${email}`, // list of receivers
+      subject: `Hello ✔ ${name} .   `, // Subject line
+      text: "Thank you for connecting 🎉. We will connect soon ☺️", // plain text body
+      html: `<b>${subject}</b> <p>your message is ${message} </p>`, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    res.send("Email send sucessfully 🎉");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Failed to send email");
+  }
+});
+
+app.listen(4000, () => {
+  console.log("App listening on port 4000");
+});
